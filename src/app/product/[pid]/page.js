e30d7c47f309f41b033da64b6f4b54c25f1e9d30
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-05-26 22:01:46 +0530

    effects,hooks are client side

--- src/app/product/[pid]/page.js
+++ src/app/product/[pid]/page.js
@@ -1,5 +1,9 @@
+"use client"
 import React from 'react';
+import { useParams } from 'next/navigation';
 export default function Product() {
+    const { pid } = useParams();
+    console.log(pid);
     return (
         <section className="py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
             <div className="max-w-screen-xl px-4 mx-auto 2xl:px-0">
